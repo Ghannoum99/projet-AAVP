2a984df375d5930366adcd00410e337a5760cd18
Ghannnoum99 <jihad.ghannoum@ens.uvsq.fr>
2022-02-25 10:16:18 +0100

    Add tas.h

---
+++ tas.h
@@ -0,0 +1,30 @@
+#ifndef _TAS_H_
+#define _TAS_H_
+
+#include <stdio.h>
+#înclude "stdlib.h"
+#include "graphe.h"
+
+typedef struct ELEMENT_TAS 
+{
+	int u;
+	double distance;
+}ELEMENT_TAS;
+
+typedef struct TAS
+{
+	ELEMENT_TAS* elements;
+	int nombre_elements;
+}TAS;
+
+/* LES OPERATIONS ELEMENTAIRES D'UN TAS */
+void ajouter_element_tas(TAS* tas, ELEMENT_TAS element);
+void supprimer_element_tas(TAS* tas, int a);
+void mise_a_jour_tas(TAS* tas, ELEMENT_TAS element);
+int tas_est_vide(TAS* tas);
+ELEMENT_TAS extraire_min_tas(TAS* tas);
+void desallouer_tas(TAS* tas);
+
+
+
+#endif
