1cccd6d471151f8c9e7fb5f59a7647ea0e2960c7
Ghannnoum99 <jihad.ghannoum@ens.uvsq.fr>
2022-02-25 10:48:59 +0100

    Add tas_fib.h

---
+++ tas_fibonacci.h
@@ -0,0 +1,42 @@
+#ifndef _TAS_FIBONACCI_H_
+#define _TAS_FIBONACCI_H_
+
+#define NIL -1
+
+typedef struct ELEMENT_TAS_FIBONACCI
+{
+	int degree;
+	float cle;
+	struct ELEMENT_TAS_FIBONACCI * pere;
+	struct ELEMENT_TAS_FIBONACCI * fils;
+	struct ELEMENT_TAS_FIBONACCI * droite;
+	struct ELEMENT_TAS_FIBONACCI * gauche;
+	
+	/*
+	 int from;
+	 int to;
+	 */
+	 
+}ELEMENT_TAS_FIBONACCI;
+
+typedef struct TAS_FIBONACCI
+{
+	int nombre_noeuds;
+	ELEMENT_TAS_FIBONACCI * min;
+	ELEMENT_TAS_FIBONACCI * racine;
+	
+}TAS_FIBONACCI;
+ 
+/* LES OPERATIONS ELEMENTAIRES POUR MANIPULER UN TAS DE FIBONACCI */
+TAS_FIBONACCI* creer_tas_fibonacci();
+ELEMENT_TAS_FIBONACCI* extraitre_min_tas_fib(TAS_FIBONACCI* tas_fib);
+void ajouter_racine_tas_fib(TAS_FIBONACCI* tas_fib, ELEMENT_TAS_FIBONACCI* noeud, int count);
+void supprimer_racine_tas_fib(TAS_FIBONACCI* tas_fib, ELEMENT_TAS_FIBONACCI* noeud, int count);
+void inserer_tas_fib(TAS_FIBONACCI* tas_fib, ELEMENT_TAS_FIBONACCI* element);
+ELEMENT_TAS_FIBONACCI* min_tas_fib(TAS_FIBONACCI* tas_fib);
+int tas_fib_est_vide(TAS_FIBONACCI* tas_fib):
+ELEMENT_TAS_FIBONACCI* nouvel_element_tas_fib(float cle, int from, int to);
+void desallouer_tas_fib(TAS_FIBONACCI* tas_fib);
+
+#endif
+
