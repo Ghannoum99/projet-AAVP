988382a74380d90e34e4112b6dd94542fcbe7aad
Ghannnoum99 <jihad.ghannoum@ens.uvsq.fr>
2022-02-27 22:23:54 +0100

    Add Dijkstra avec tas de fib

--- dijkstra.c
+++ dijkstra.c
@@ -35,7 +35,7 @@ void dijkstra_tas(GRAPHE* graphe, int sommet_src, int* predecesseur,double* poid
 	{
 		tmp = extraire_min_tas(&tas);
 		u = tmp.u;
-		marquer_noeud(etat,u, VISITE);
+		marquer_noeud(etat,u, DEJA_VISITE);
 		
 		//verifier tous les arcs sortant de u
 		n = graphe->LISTE_ADJACENCE[u].nombre_elements;
@@ -44,7 +44,7 @@ void dijkstra_tas(GRAPHE* graphe, int sommet_src, int* predecesseur,double* poid
 		for (int i = 0; i < n; i++)
 		{
 			v = arete->noeud;
-			if (!get_etat(etat, v, VISITE))
+			if (!get_etat(etat, v, DEJA_VISITE))
 			{
 				dist = poids[u] + arete->poids;
 				if (dist < poids[v])
@@ -62,7 +62,76 @@ void dijkstra_tas(GRAPHE* graphe, int sommet_src, int* predecesseur,double* poid
 	}
 	
 	desallouer_etat(etat);
-	free(tas.elements);
+	desallouer_tas(tas);
 	
+}
+
+
+void dijkstra_tas_fib(GRAPHE* graphe, int sommet_src, int* predec, doouble* poids)
+{
+	LISTE_ADJACENCE_NOEUD* arete;
+	ELEMENT_TAS_FIBONACCI* tmp;
+	TAS_FIBONACCI* tas_fib = creer_tas_fibonacci();
+	double dist;
+	int u, v, n, x;
+	ETAT* etat = initialiser_etat(graphe);
+	
+	
+	for (int i = 0; i < graphe->nombre_elements; i++)
+	{
+		if (i != sommet_src)
+			poids[i] = -1;
+		else
+			poids[i] = 0;
+			
+		predec[i] = INT_MAX;
+	}
+	
+	marquer_noeud(etat,u, DEJA_VISITE);
+	
+	//ajouter les noeuds dest de tous les arcs sortant de sommet_src au tas de fib
+	arete = graphe->LISTE_ADJACENCE[sommet_src].racine;
+	n = graphe->LISTE_ADJACENCE[sommet_src].nombre_elements;
+	
+	for (int i = 0; i < n; i++)
+	{
+		dist = poids[sommet_src] + rete->poids;
+		v = arete->noeud;
+		inserer_tas_fib(tas_fib, nouvel_element_tas_fib(dist, sommet_src, v));
+		
+		arete = arete->suivant;
+	}
+	
+	while (! tas_fib_est_vide(tas_fib))
+	{
+		tmp = extraire_min_tas_fib(tas_fib);
+		
+		u = tmp->from;
+		v = tmp->to;
+		dist = tmp->cle;
+		
+		if (!get_etat(etat, v, DEJA_VISITE)
+		{
+			poids[v] = dist;
+			predec[v] = u;
+			marquer_noeud(etat, v, DEJA_VISITE);
+			
+			arete = graphe->LISTE_ADJACENCE[v].racine;
+			n = graphe->LISTE_ADJACENCE[v].nombre_elements;
+			
+			for (int i = 0; i < n; i++)
+			{
+				x = arete->noeud;
+				dist = poids[v] + arete->poids;
+				inserer_tas_fib(tas_fib, nouvel_element_tas_fib(dist, v, x);
+				
+				arete = arete->suivant;
+			}
+			
+		}
+	}
+	
+	desallouer_etat(etat);
+	desallouer_tas_fib(tas_fib);
 	
 }
